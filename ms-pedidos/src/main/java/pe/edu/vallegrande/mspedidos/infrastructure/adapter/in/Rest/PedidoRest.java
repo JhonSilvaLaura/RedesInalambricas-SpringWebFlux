3e261f43474c9ba3eaf75bca1114c61d2a5ef30f
JhonSilva <jhon.silva.laura@vallegrande.edu.pe>
2026-06-26 12:51:14 -0500

    crud

---
+++ ms-pedidos/src/main/java/pe/edu/vallegrande/mspedidos/infrastructure/adapter/in/Rest/PedidoRest.java
@@ -0,0 +1,31 @@
+package pe.edu.vallegrande.mspedidos.infrastructure.adapter.in.Rest;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.vallegrande.mspedidos.application.port.in.IPedidoServicePort;
+import pe.edu.vallegrande.mspedidos.domain.model.Pedido;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api/pedidos")
+@RequiredArgsConstructor
+public class PedidoRest {
+
+    private final IPedidoServicePort servicePort;
+
+    @GetMapping
+    public Flux<Pedido> findAll() {
+        return servicePort.findALl();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<Pedido> findById(@PathVariable Long id) {
+        return servicePort.finById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public
+}
